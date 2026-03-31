194ee5ac41d364eb312cd4971e1c1aaed3778ce5
Ibtihel Baccari <baccari.ibtihel@esprit.tn>
2026-03-31 06:20:35 +0100

    Refactor PiApplicationTests by removing Spring context
    Removed Spring Boot test annotation and added a comment for clarity.

--- src/test/java/tn/esprit/_4se2/pi/PiApplicationTests.java
+++ src/test/java/tn/esprit/_4se2/pi/PiApplicationTests.java
@@ -1,13 +1,11 @@
 package tn.esprit._4se2.pi;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class PiApplicationTests {
 
     @Test
     void contextLoads() {
+        // Test simple sans Spring context
     }
-
 }
