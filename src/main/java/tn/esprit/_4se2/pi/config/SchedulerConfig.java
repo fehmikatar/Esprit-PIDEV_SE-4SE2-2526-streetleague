5ac38face286d456a6d9dddbff46d5f4b130a644
fehmikatar <fehmi.katar@esprit.tn>
2026-05-01 15:39:56 +0100

    feat: integrate e-commerce modules (Cart, Product, Favorite, Category, PromoCode) from ibtihel

---
+++ src/main/java/tn/esprit/_4se2/pi/config/SchedulerConfig.java
@@ -0,0 +1,9 @@
+package tn.esprit._4se2.pi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}
