56648e6334ae4236cfc8b606de1d4b61fa6ca921
naifour <mahdinaifer6@gmail.com>
2026-05-02 17:03:37 +0100

    comp

--- src/main/java/tn/esprit/_4se2/pi/Enum/MatchEventType.java
+++ src/main/java/tn/esprit/_4se2/pi/Enum/MatchEventType.java
@@ -1,14 +1,5 @@
 package tn.esprit._4se2.pi.Enum;
 
 public enum MatchEventType {
-    GOAL,
-    SCORE,
-    ASSIST,
-    FOUL,
-    SUBSTITUTION,
-    YELLOW_CARD,
-    WARNING,
-    RED_CARD,
-    EJECTION,
-    OTHER
+    SCORE, GOAL, ASSIST, FOUL, SUBSTITUTION, WARNING, YELLOW_CARD, EJECTION, RED_CARD, TIMEOUT, OTHER
 }
--- src/main/java/tn/esprit/_4se2/pi/repositories/CompetitionRepository.java
+++ src/main/java/tn/esprit/_4se2/pi/repositories/CompetitionRepository.java
@@ -1,11 +1,7 @@
 package tn.esprit._4se2.pi.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import tn.esprit._4se2.pi.Enum.CompetitionStatus;
 import tn.esprit._4se2.pi.entities.Competition;
 
-import java.util.List;
-
 public interface CompetitionRepository extends JpaRepository<Competition, Long> {
-    List<Competition> findByStatus(CompetitionStatus status);
 }
