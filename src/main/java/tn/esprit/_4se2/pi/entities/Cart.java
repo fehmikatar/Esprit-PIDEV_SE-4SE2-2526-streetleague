60ec86cb005f25bafe6d0e5203f3a886dda8a3f3
fehmi katar <167518583+fehmikatar@users.noreply.github.com>
2026-05-05 11:28:47 +0100

    Refactor Cart class to include additional fields

--- src/main/java/tn/esprit/_4se2/pi/entities/Cart.java
+++ src/main/java/tn/esprit/_4se2/pi/entities/Cart.java
@@ -1,18 +1,7 @@
 package tn.esprit._4se2.pi.entities;
 
-
-import lombok.Getter;
-import lombok.Setter;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.List;
-
-@Getter
-@Setter
-public class Cart {
-package tn.esprit._4se2.pi.entities;
-
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -25,120 +14,52 @@ public class Cart {
     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
     private List<CartItem> items;
 
-    // Constructeurs
+    private Double deliveryFee;
+    private String appliedPromoCode;
+    private String status;
+    private LocalDateTime createdAt;
+    private LocalDateTime lastModified;
+
+    private String clientName;
+    private String clientAddress;
+    private String clientPostalCode;
+    private String clientCity;
+    private String clientPhone;
+    private String clientEmail;
+
+    private String deliveryMode;
+    private String paymentMode;
+    private String cardNumber;
+    private String expiryDate;
+    private String cvv;
+    private String deliveryConfirmationCode;
+
+    // Constructeur par défaut
     public Cart() {}
 
     // Getters et setters
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public List<CartItem> getItems() { return items; }
+    public void setItems(List<CartItem> items) { this.items = items; }
 
-    public List<CartItem> getItems() {
-        return items;
-    }
+    public Double getDeliveryFee() { return deliveryFee; }
+    public void setDeliveryFee(Double deliveryFee) { this.deliveryFee = deliveryFee; }
 
-    public void setItems(List<CartItem> items) {
-        this.items = items;
-    }
-}
+    public String getAppliedPromoCode() { return appliedPromoCode; }
+    public void setAppliedPromoCode(String appliedPromoCode) { this.appliedPromoCode = appliedPromoCode; }
 
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
 
-    // Method to get the items in the cart
-    public List<CartItem> getItems() {
-        return items;
-    }
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
 
-    // Method to get the total price of the cart
-    public BigDecimal getTotal() {
-        return total;
-    }
+    public LocalDateTime getLastModified() { return lastModified; }
+    public void setLastModified(LocalDateTime lastModified) { this.lastModified = lastModified; }
 
-    // Method to get the delivery fee
-    public BigDecimal getDeliveryFee() {
-        return deliveryFee;
-    }
+    public String getClientName() { return clientName; }
+    public void setClientName(String clientName) { this.clientName = clientName; }
 
-    // Method to get the applied promo code
-    public String getAppliedPromoCode() {
-        return appliedPromoCode;
-    }
-
-    // Method to get the cart status
-    public CartStatus getStatus() {
-        return status;
-    }
-
-    // Method to get the creation date of the cart
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    // Method to get the last modified date of the cart
-    public LocalDateTime getLastModified() {
-        return lastModified;
-    }
-
-    // Method to get the client's name
-    public String getClientName() {
-        return clientName;
-    }
-
-    // Method to get the client's address
-    public String getClientAddress() {
-        return clientAddress;
-    }
-
-    // Method to get the client's postal code
-    public String getClientPostalCode() {
-        return clientPostalCode;
-    }
-
-    // Method to get the client's city
-    public String getClientCity() {
-        return clientCity;
-    }
-
-    // Method to get the client's phone
-    public String getClientPhone() {
-        return clientPhone;
-    }
-
-    // Method to get the delivery mode
-    public String getDeliveryMode() {
-        return deliveryMode;
-    }
-
-    // Method to get the payment mode
-    public String getPaymentMode() {
-        return paymentMode;
-    }
-
-    // Method to get the card number
-    public String getCardNumber() {
-        return cardNumber;
-    }
-
-    // Method to get the expiry date
-    public String getExpiryDate() {
-        return expiryDate;
-    }
-
-    // Method to get the CVV
-    public String getCvv() {
-        return cvv;
-    }
-
-    // Method to get the client's email
-    public String getClientEmail() {
-        return clientEmail;
-    }
-
-    // Method to get the delivery confirmation code
-    public String getDeliveryConfirmationCode() {
-        return deliveryConfirmationCode;
-    }
-}
+    public String getClient
