c816b273670ed9aefc58bf97e01fc207d41f4583
fehmi katar <167518583+fehmikatar@users.noreply.github.com>
2026-05-05 11:27:20 +0100

    Update Cart.java

--- src/main/java/tn/esprit/_4se2/pi/entities/Cart.java
+++ src/main/java/tn/esprit/_4se2/pi/entities/Cart.java
@@ -10,28 +10,42 @@ import java.util.List;
 @Getter
 @Setter
 public class Cart {
+package tn.esprit._4se2.pi.entities;
 
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String orderCode;
-    private List<CartItem> items;  // Assuming CartItem is another class
-    private BigDecimal total;
-    private BigDecimal deliveryFee;
-    private String appliedPromoCode;
-    private CartStatus status;
-    private LocalDateTime createdAt;
-    private LocalDateTime lastModified;
-    private String clientName;
-    private String clientAddress;
-    private String clientPostalCode;
-    private String clientCity;
-    private String clientPhone;
-    private String deliveryMode;
-    private String paymentMode;
-    private String cardNumber;
-    private String expiryDate;
-    private String cvv;
-    private String clientEmail;
-    private String deliveryConfirmationCode;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+    private List<CartItem> items;
+
+    // Constructeurs
+    public Cart() {}
+
+    // Getters et setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<CartItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<CartItem> items) {
+        this.items = items;
+    }
+}
+
 
     // Method to get the items in the cart
     public List<CartItem> getItems() {
